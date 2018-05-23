931d3a655da5b3d94871c5c93b3f6d8c0d1a6403
Mario <marioisbeck@gmail.com>
2018-05-23 17:28:34 +0200

    commiting some missing documents

---
+++ node-mongoose/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+// following line instead of the commented once, as
+// useMongoClient is no longer required by mongoose
+const connect = mongoose.connect(url)
+// const connect = mongoose.connect(url, {
+//     useMongoClient: true
+// });
+
+// adjusted the following two lines because of mongo v3.x
+connect.then((db) => {
+    var db = mongoose.connection;
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Fun6',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return db.collection('dishes').drop();
+        })
+        .then(() => {
+            return db.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
