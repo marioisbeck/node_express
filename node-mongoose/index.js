1d7b0c7d43bc1a021b28702a41c8731b5fec1b32
Mario <marioisbeck@gmail.com>
2018-05-23 17:37:28 +0200

    node-mongoose exercise 1 part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -18,27 +18,25 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Fun6',
+    Dishes.create({
+        name: 'Fun7',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
