87004d7dfc9242163108552eab3ebc5c00d45990
Mario <marioisbeck@gmail.com>
2018-05-23 16:14:37 +0200

    node-mongo example 1 part b

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
@@ -11,28 +12,55 @@ MongoClient.connect(url, (err, client) => {
 
     console.log('Connected correctly to server');
 
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+                            
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection:", result);
 
-                // had to change db.close() to client.close()
-                client.close();
+                                // mongodb 3.x update db.close() -> client.close()
+                                client.close();
+                            });
+                        });
+                    });
             });
-        });
     });
+
+    // used in git commit: "node-mongo exercise 1 part a"
+    // const collection = db.collection("dishes");
+    // collection.insertOne({"name": "Uthappizza", "description": "test"},
+    // (err, result) => {
+    //     assert.equal(err,null);
+
+    //     console.log("After Insert:\n");
+    //     console.log(result.ops);
+
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
+            
+    //         console.log("Found:\n");
+    //         console.log(docs);
+
+    //         db.dropCollection("dishes", (err, result) => {
+    //             assert.equal(err,null);
+
+    //             // had to change db.close() to client.close()
+    //             client.close();
+    //         });
+    //     });
+    // });
 
 });
 
