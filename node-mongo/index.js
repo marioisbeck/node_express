c8d02d95ac991aa9da4b41b1abfe10f3afc00bf0
Mario <marioisbeck@gmail.com>
2018-05-23 16:49:16 +0200

    callback hell and promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,65 +4,109 @@ const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-// had to add the following two lines to make the example work with mongoDB version: v3.6.5 
-MongoClient.connect(url, (err, client) => {
-    var db = client.db('mytestingdb');
 
-    assert.equal(err,null);
+// had to add the following two lines to make the example work with mongoDB version: v3.6.5 
+MongoClient.connect(url).then((client) => {
+    var db = client.db('conFusion');
 
     console.log('Connected correctly to server');
 
     dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
+        "dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
                             
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection:", result);
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
 
-                                // mongodb 3.x update db.close() -> client.close()
-                                client.close();
-                            });
-                        });
-                    });
-            });
-    });
+            // mongoDB v3.x: db.close() -> client.close()
+            return client.close();
+        })
+        .catch((err) => console.log(err));
 
-    // used in git commit: "node-mongo exercise 1 part a"
-    // const collection = db.collection("dishes");
-    // collection.insertOne({"name": "Uthappizza", "description": "test"},
-    // (err, result) => {
-    //     assert.equal(err,null);
+})
+.catch((err) => console.log(err));
 
-    //     console.log("After Insert:\n");
-    //     console.log(result.ops);
+// // used in git commit: "node-mongo exercise 1"
+// // had to add the following two lines to make the example work with mongoDB version: v3.6.5 
+// MongoClient.connect(url, (err, client) => {
+//     var db = client.db('conFusion');
 
-    //     collection.find({}).toArray((err, docs) => {
-    //         assert.equal(err,null);
+//     assert.equal(err,null);
+
+//     console.log('Connected correctly to server');
+
+//     dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+//         "dishes", (result) => {
+//             console.log("Insert Document:\n", result.ops);
+
+//             dboper.findDocuments(db, "dishes", (docs) => {
+//                 console.log("Found Documents:\n", docs);
+
+//                 dboper.updateDocument(db, { name: "Vadonut" },
+//                     { description: "Updated Test" }, "dishes",
+//                     (result) => {
+//                         console.log("Updated Document:\n", result.result);
+
+//                         dboper.findDocuments(db, "dishes", (docs) => {
+//                             console.log("Found Updated Documents:\n", docs);
+                            
+//                             db.dropCollection("dishes", (result) => {
+//                                 console.log("Dropped Collection:", result);
+
+//                                 // mongodb 3.x update db.close() -> client.close()
+//                                 client.close();
+//                             });
+//                         });
+//                     });
+//             });
+//     });
+
+// });
+
+//     // used in git commit: "node-mongo exercise 1 part a"
+//     const collection = db.collection("dishes");
+//     collection.insertOne({"name": "Uthappizza", "description": "test"},
+//     (err, result) => {
+//         assert.equal(err,null);
+
+//         console.log("After Insert:\n");
+//         console.log(result.ops);
+
+//         collection.find({}).toArray((err, docs) => {
+//             assert.equal(err,null);
             
-    //         console.log("Found:\n");
-    //         console.log(docs);
+//             console.log("Found:\n");
+//             console.log(docs);
 
-    //         db.dropCollection("dishes", (err, result) => {
-    //             assert.equal(err,null);
+//             db.dropCollection("dishes", (err, result) => {
+//                 assert.equal(err,null);
 
-    //             // had to change db.close() to client.close()
-    //             client.close();
-    //         });
-    //     });
-    // });
+//                 // had to change db.close() to client.close()
+//                 client.close();
+//             });
+//         });
+//     });
 
-});
+// });
 
 
 // // Keep the URL intact, but add the following variable to your connect function.
