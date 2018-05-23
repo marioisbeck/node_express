f69060fbd77841774ee07fec0802f1ae5e01bd6d
Mario <marioisbeck@gmail.com>
2018-05-23 10:26:21 +0200

    first assignment

--- node-express/index.js
+++ node-express/index.js
@@ -1,51 +1,57 @@
-const express = require('express'),
-     http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const hostname = 'localhost';
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+
+const hostname = "localhost";
 const port = 3000;
 
-const dishRouter = require('./routes/dishRouter');
-
 const app = express();
-
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use('/dishes', dishRouter);
 
-// app.get('/dishes/:dishId', (req,res,next) => {
-//     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
+// app.get("/dishes/:dishId", (req,res,next) => {
+//  res.end("Will send details of the dish: "
+//    + req.params.dishId + "to you!");
 // });
 
-// app.post('/dishes/:dishId', (req, res, next) => {
-//   res.statusCode = 403;
-//   res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+// app.post("/dishes/:dishId", (req,res,next) => {
+//  res.statusCode = 403;
+//  res.end("POST operation not supported on /dishes/ "
+//    + req.params.dishId);
 // });
 
-// app.put('/dishes/:dishId', (req, res, next) => {
-//   res.write('Updating the dish: ' + req.params.dishId + '\n');
-//   res.end('Will update the dish: ' + req.body.name + 
-//         ' with details: ' + req.body.description);
+// app.put("/dishes/:dishId", (req,res,next) => {
+//  res.write("Updating the dish: " + req.params.dishId + "\n");
+//  res.end("Will update the dish: " 
+//    + req.body.name + " with details: " + req.body.description);
 // });
 
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
+// app.delete("/dishes/:dishId", (req,res,next) => {
+//  res.end("Deleting dish: " + req.params.dishId);
 // });
 
-app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  // console.log(req.headers); // line not needed when morgan is set up. sufficient logging done by morgan
+app.use(express.static(__dirname + "/public"))
+
+app.use((req,res,next) => {
+  // console.log(req.headers);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an express server</h1></body></html>");
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
