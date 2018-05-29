070f03169b7d3ec81a8007f77abab309e97992b4
Mario <marioisbeck@gmail.com>
2018-05-29 14:42:18 +0200

    Express Sessions Part 2

--- conFusion/app.js
+++ conFusion/app.js
@@ -50,37 +50,26 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      req.session.user = 'admin';
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
-  } else {
-    if (req.session.user == 'admin') {
-      console.log('req.session: ', req.session);
+    // var authHeader = req.headers.authorization;
+    // if (!authHeader) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+    return;
+  }
+  else {
+    if (req.session.user == 'authenticated') {
       next();
     } else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       next(err);
     }
   }
@@ -156,8 +145,6 @@ app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
@@ -171,7 +158,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') == 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
