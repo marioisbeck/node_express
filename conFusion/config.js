4502a1c2f33d72c4b4876e8d974cfd97cda44c23
Mario Beck <marioisbeck@gmail.com>
2018-06-06 16:06:15 +0200

    OAuth Facebook - does not work fully...

--- conFusion/config.js
+++ conFusion/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '2078366682486494',
+    clientSecret: 'ee82f05d5322c623c6d1f536ddd42e4f'
+  }
 }
