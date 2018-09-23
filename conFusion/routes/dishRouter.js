63189342063020de56fe0d6c1f5a00ee197b677c
Mario <marioisbeck@gmail.com>
2018-09-23 09:11:27 +0200

    Integrating Client and Server

--- conFusion/routes/dishRouter.js
+++ conFusion/routes/dishRouter.js
@@ -13,7 +13,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, (req, res, next) => {
-    Dishes.find({})
+    Dishes.find(req.query)
         .populate('comments.author')
         .then((dishes) => {
             res.statusCode = 200;
@@ -111,9 +111,13 @@ dishRouter.route('/:dishId/comments')
                 dish.comments.push(req.body);
                 dish.save()
                     .then((dish) => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(dish);
+                        Dishes.findById(dish._id)
+                        .populate('comments.author')
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        })
                     }, (err) => next(err));
             } else {
                 err = new Error('Dish ' + req.params.dishId + ' not found');
@@ -190,11 +194,15 @@ dishRouter.route('/:dishId/comments/:commentId')
                 dish.comments.id(req.params.commentId).comment = req.body.comment;                
             }
             dish.save()
-            .then((dish) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(dish);                
-            }, (err) => next(err));
+                .then((dish) => {
+                    Dishes.findById(dish._id)
+                        .populate('comments.author')
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        })
+                }, (err) => next(err));
         }
         else if (dish == null) {
             err = new Error('Dish ' + req.params.dishId + ' not found');
@@ -215,11 +223,15 @@ dishRouter.route('/:dishId/comments/:commentId')
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             dish.comments.id(req.params.commentId).remove();
             dish.save()
-            .then((dish) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(dish);                
-            }, (err) => next(err));
+                .then((dish) => {
+                    Dishes.findById(dish._id)
+                        .populate('comments.author')
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        })
+                }, (err) => next(err));
         }
         else if (dish == null) {
             err = new Error('Dish ' + req.params.dishId + ' not found');
--- conFusion/routes/favoriteRouter.js
+++ conFusion/routes/favoriteRouter.js
@@ -95,8 +95,37 @@ favoriteRouter.route('/')
 favoriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
 .get(cors.cors, authenticate.verifyUser, (req,res,next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on /favorites/' + req.params.dishId);
+    Favorites.findOne({
+            user: req.user._id
+        })
+        .then((favorites) => {
+            if (!favorites) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({
+                    "exists": false,
+                    "favorites": favorites
+                });
+            } else {
+                if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    return res.json({
+                        "exists": false,
+                        "favorites": favorites
+                    });
+                } else {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    return res.json({
+                        "exists": true,
+                        "favorites": favorites
+                    });
+                }
+            }
+
+        }, (err) => next(err))
+        .catch((err) => next(err))
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
     Favorites.findOne({user: req.user._id}, (err, favorite) =>
@@ -145,11 +174,17 @@ favoriteRouter.route('/:dishId')
             if (index != -1){
                 favorite.dishes.splice(index, 1);
                 favorite.save()
-                .then((favorite) => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(favorite);
-                }, (err) => next(err));
+                    .then((favorite) => {
+                        Favorites.findById(favorite._id)
+                            .populate('user')
+                            .populate('dishes')
+                            .then((favorite) => {
+                                console.log('Favorite Dish Deleted!', favorite);
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(favorite);
+                            })
+                    })
             }
             else {
                 err = new Error('Dish ' + req.params.dishId + 
